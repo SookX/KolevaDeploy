11b9168a577f3934b73f390ba37d9a7855551bd4
MrTiNnN <martin.v.velchev@gmail.com>
2024-11-07 20:36:45 +0200

    Add articles

--- server/client/src/pages/Home/components/About/components/Articles.jsx
+++ server/client/src/pages/Home/components/About/components/Articles.jsx
@@ -1,6 +1,8 @@
 import Article from "./Article"
 import article1 from '../../../../../img/article1.jpg'
 import article2 from '../../../../../img/article2.jpg'
+import article3 from '../../../../../img/article3.jpg'
+import article4 from '../../../../../img/article4.jpg'
 import SectionHeading from "../../../../../components/SectionHeading/SectionHeading"
 
 const Articles = () => {
@@ -22,6 +24,24 @@ const Articles = () => {
                     website='botevgrad.com'
                     link='https://botevgrad.com/news/91570/Akcenti-ot-lekciyata-na-kardiologa-d-r-Kalina-Koleva-pred-botevgradchani/'
                     image={article2}
+                />
+            </div>
+
+            <div className="articles">
+                <Article
+                    title='Лекарят, който напусна Германия, за да лекува в Ботевград.'
+                    text={`"Работя медицина с цялото си сърце, плам и желание. За мен е любима професия, която усещам като призвание."`}
+                    website='Ботевградски Вести Плюс'
+                    link='https://www.facebook.com/story.php?story_fbid=450932334641748&id=100091749549113'
+                    image={article3}
+                />
+
+                <Article
+                    title='Специализиран кабинет по кардиология и съдова медицина'
+                    text='Специализиран кабинет по кардиология и съдова медицина отваря врати в МБАЛ – Ботевград ЕООД. Кабинетът разполага с уникална за областта апаратура с така наречения тъканен доплер за сърце.'
+                    website='Вестник Правешки глас'
+                    link='https://www.facebook.com/pravets.news/posts/pfbid032B4BGezVGfJeKWJu8wdTaiac87GG8Vn6dcz8HDZ2XCredLcg6SFemnoJRJEpqBxVl'
+                    image={article4}
                 />
             </div>
         </div>
