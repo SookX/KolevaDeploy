0db4b084e6acb1036313ece9773015cb130f6839
MrTiNnN <martin.v.velchev@gmail.com>
2024-11-10 21:08:31 +0200

    Fix gallery problem fr fr

--- server/client/src/pages/Gallery/Gallery.jsx
+++ server/client/src/pages/Gallery/Gallery.jsx
@@ -1,97 +1,19 @@
-import { useContext, useEffect, useState } from 'react'
+import { useState } from 'react'
 import './gallery.less'
-import { DataContext } from '../../context/DataContext'
-import { CgMathPlus, CgTrash } from 'react-icons/cg'
 import OpenPhoto from './OpenPhoto'
+import gallery1 from '../../img/gallery1.jpg'
+import gallery2 from '../../img/gallery2.jpg'
+import gallery3 from '../../img/gallery3.jpg'
 
 const Gallery = () => {
-    // Gets global data from the context
-    const { crud, access, url } = useContext(DataContext)
-
-
-
     // Holds the state for the photos
-    const [photos, setPhotos] = useState([])
+    const photos = [gallery1, gallery2, gallery3]
     const [selectedPhoto, setSelectedPhoto] = useState(false)
-
-
-
-    // Holds the error and loading state of the gallery
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
-
-
-
-    // Gets the photos from the backend on init
-    const fetchPhotos = async () => {
-        setLoading(true)
-
-        const response = await crud({
-            url: '/service/gallery/',
-            method: 'get'
-        })
-
-        if(response.status == 200) setPhotos(response.data.data.images)
-        else setError('Нещо се обърка. Моля, опитайте по-късно.')
-
-        setLoading(false)
-    }
-
-
-    useEffect(() => {
-        fetchPhotos()
-    }, [])
-
-
-
-    // Makes a crud operation to the backend with the new photo
-    const handleUpload = async (e) => {
-        setLoading(true)
-
-        const formData = new FormData()
-        formData.append("image", e.target.files[0])
-        
-        const response = await crud({
-            url: '/service/gallery/',
-            method: 'post',
-            body: formData,
-        })
-
-        if(response.status == 201) fetchPhotos()
-        else {
-            setError('Нещо се обърка. Опитай пак.')
-            setLoading(false)
-        }
-    }
-
-
-
-    // Makes a crud operation to the backend to delete the photo
-    const handleDelete = async (id) => {
-        setLoading(true)
-
-        const response = await crud({
-            url: `/service/gallery/${id}`,
-            method: 'delete'
-        })
-
-        if(response.status == 204) fetchPhotos()
-        else setError('Нещо се обърка. Опитай пак.')
-
-        setLoading(false)
-    }
 
 
 
     return (
         <section className="section-gallery">
-            {
-                loading &&
-                <div className="overlay">
-                    <div className="heart"></div>
-                </div>
-            }
-
 
             <OpenPhoto
                 isOpen={selectedPhoto}
@@ -105,26 +27,7 @@ const Gallery = () => {
                     <h2 className="title">Галерия</h2>
                     <p className='text'>Тук може да разгледате снимки от кабинета.</p>
                 </div>
-
-                {
-                    access &&
-                    <label htmlFor='upload' className="gallery-upload">
-                        <input
-                            type="file"
-                            id='upload'
-                            accept="image/*"
-                            onChange={(e) => handleUpload(e)}
-                        />
-                        <CgMathPlus />
-                        <p>Качване</p>
-                    </label>
-                }
             </div>
-
-            {
-                error &&
-                <p className="error">{error}</p>
-            }
 
             <div className="gallery-grid">
                 {
@@ -133,15 +36,10 @@ const Gallery = () => {
                         <div className='grid-photo-container'>
                             <img
                                 key={i}
-                                src={`${url}/${photo.image}`}
+                                src={photo}
                                 className='grid-photo'
-                                onClick={() => setSelectedPhoto(`${url}/${photo.image}`)}
+                                onClick={() => setSelectedPhoto(photo)}
                             />
-
-                            {
-                                access &&
-                                <CgTrash onClick={() => handleDelete(photo._id)} className='grid-photo-icon' />
-                            }
                         </div>
                     ))
                 }
