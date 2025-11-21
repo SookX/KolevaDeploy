d065dcdc73776ab411190553b135becaff2ce11d
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-11-21 19:43:12 +0200

    Add interview 3

--- server/client/src/pages/Home/components/About/components/Interviews.jsx
+++ server/client/src/pages/Home/components/About/components/Interviews.jsx
@@ -4,6 +4,7 @@ import interview2 from "../../../../../video/interview2.mp4"
 const Interviews = () => {
     return (
         <div className="interview-container">
+            <Interview src="https://media.botevgrad.com/2/29403/video.mp4" />
             <Interview src={interview2} />
             <Interview src="https://media.botevgrad.com/2/28499/video.mp4" />
         </div>
